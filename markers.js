cad7ce830ce378031b2c547fbecfdb8f75f7a71b
bowenr1 <bowenr1@protonmail.com>
2024-06-14 21:47:57 -0400

    Update markers.js

--- markers.js
+++ markers.js
@@ -6,7 +6,7 @@ var markers = [
         excerpt: "This is an excerpt for Article 1.",
         tags: "tag1, tag2, tag3",
         url: "https://example.com/article1",
-        imageUrl: "https://example.com/image1.jpg"
+        imageUrl: "https://pulsemagazine.sx/wp-content/uploads/2024/06/cropped-cropped-PULSE_logo-200x47.png"
     },
     {
         lat: 18.0500,
