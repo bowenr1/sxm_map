56e74068584a58c2da7e7effed9d3fa8b96a90fa
bowenr1 <bowenr1@protonmail.com>
2024-06-14 21:07:37 -0400

    Update markers.js

--- markers.js
+++ markers.js
@@ -3,7 +3,7 @@ var markers = [
         lat: 18.0425,
         lng: -63.0548,
         title: "Article 1",
-        excerpt: "This is an excerpt for Article 1.",
+        excerpt: "This is an excerpt for Article 1.***",
         url: "https://pulsemagazine.sx/stories",
         tags: "tag1", "tag2"
     },
